88ea2d9a02b762d7076e1a479608f9e53c3695a8
ALIMUZZAMAN <alimuzzamanzamaddar@gmail.com>
2023-05-20 02:48:24 +0600

    category section is done

--- src/Pages/Blogs/Blogs.jsx
+++ src/Pages/Blogs/Blogs.jsx
@@ -4,7 +4,8 @@ import useTitle from "../../Hooks/useTitle";
 const Blogs = () => {
   useTitle('Blogs')
   return (
-    <div>
+    <div className="my-14">
+         <h1 className="text-6xl font-bold text-center py-5">Question answer section is here ??</h1>
         <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
           <div className="collapse-title text-xl font-medium">
            1. What is an access token and refresh token? How do they work and where should we store them on the client-side?
@@ -17,10 +18,12 @@ const Blogs = () => {
         </div>
         <div tabIndex={1} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
           <div className="collapse-title text-xl font-medium">
-            Focus me to see content
+            2 .Compare SQL and NoSQL databases?
           </div>
           <div className="collapse-content">
-            <p>tabIndex={1} attribute is necessary to make the div focusable</p>
+            <p>sql databases are vertically scalable, while nosql databases are horizontally scalable. sql databases are table-based, while nosql databases are document, key-value, graph, or wide-column stores. sql databases are better for multi-row transactions, while nosql is better for unstructured data like documents or json
+            nosql stands for Not only sql. It is a type of database that uses non-relational data structures, such as documents, graph databases, and key-value stores to store and retrieve data. nosql systems are designed to be more flexible than traditional relational databases and can scale up or down easily to accommodate changes in usage or load. This makes them ideal for use in applications
+            </p>
           </div>
         </div>
         <div tabIndex={2} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
