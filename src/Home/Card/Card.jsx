724de7db1290ac3429aa8f0eece1890dfb865fee
ALIMUZZAMAN <alimuzzamanzamaddar@gmail.com>
2023-05-20 19:29:30 +0600

    firebase deployment complete

---
+++ src/Home/Card/Card.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Card.css'
+
+const Card = () => {
+    return (
+        <div className='py-12'>
+            <div className="toy-card">
+                <img src="https://www.joblo.com/wp-content/uploads/2023/04/Fs0ObfgX0AMLSTL-1024x777.jpeg" alt="" />
+            </div>
+            <div className="toy-card">
+                <img src="https://www.joblo.com/wp-content/uploads/2023/04/Fs0ObfgX0AMLSTL-1024x777.jpeg" alt="" />
+            </div>
+            <div className="toy-card">
+                <img src="https://www.joblo.com/wp-content/uploads/2023/04/Fs0ObfgX0AMLSTL-1024x777.jpeg" alt="" />
+            </div>
+            <div className="toy-card">
+                <img src="https://www.joblo.com/wp-content/uploads/2023/04/Fs0ObfgX0AMLSTL-1024x777.jpeg" alt="" />
+            </div>
+            <div className="toy-card">
+                <img src="https://www.joblo.com/wp-content/uploads/2023/04/Fs0ObfgX0AMLSTL-1024x777.jpeg" alt="" />
+            </div>
+            <div className="toy-card">
+                <img src="https://www.joblo.com/wp-content/uploads/2023/04/Fs0ObfgX0AMLSTL-1024x777.jpeg" alt="" />
+            </div>
+            
+        </div>
+    );
+};
+
+export default Card;
