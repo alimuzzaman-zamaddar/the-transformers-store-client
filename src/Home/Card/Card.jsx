bb0b11a59bfba95d517d386f226ca0b6e41fe140
ALIMUZZAMAN <alimuzzamanzamaddar@gmail.com>
2023-05-20 20:30:37 +0600

    extra two section is done

--- src/Home/Card/Card.jsx
+++ src/Home/Card/Card.jsx
@@ -1,30 +1,71 @@
-import React from 'react';
-import './Card.css'
+import React from "react";
+import "./Card.css";
+import{ useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+
 
 const Card = () => {
-    return (
-        <div className='py-12'>
-            <div className="toy-card">
-                <img src="https://www.joblo.com/wp-content/uploads/2023/04/Fs0ObfgX0AMLSTL-1024x777.jpeg" alt="" />
-            </div>
-            <div className="toy-card">
-                <img src="https://www.joblo.com/wp-content/uploads/2023/04/Fs0ObfgX0AMLSTL-1024x777.jpeg" alt="" />
-            </div>
-            <div className="toy-card">
-                <img src="https://www.joblo.com/wp-content/uploads/2023/04/Fs0ObfgX0AMLSTL-1024x777.jpeg" alt="" />
-            </div>
-            <div className="toy-card">
-                <img src="https://www.joblo.com/wp-content/uploads/2023/04/Fs0ObfgX0AMLSTL-1024x777.jpeg" alt="" />
-            </div>
-            <div className="toy-card">
-                <img src="https://www.joblo.com/wp-content/uploads/2023/04/Fs0ObfgX0AMLSTL-1024x777.jpeg" alt="" />
-            </div>
-            <div className="toy-card">
-                <img src="https://www.joblo.com/wp-content/uploads/2023/04/Fs0ObfgX0AMLSTL-1024x777.jpeg" alt="" />
-            </div>
-            
+    useEffect(() => {
+        AOS.init();
+        
+      }, []);
+  return (
+    <div className="group-title">
+        <h1 className="font-extrabold text-5xl text-center py-11">Variety of Toy Section</h1>
+      <div className="py-12  card-container ">
+        <div className="toy-card" data-aos="fade-left" data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="1000"  >
+          <img
+            src="https://www.joblo.com/wp-content/uploads/2023/04/Fs0ObfgX0AMLSTL-1024x777.jpeg"
+            alt=""
+          />
         </div>
-    );
+        <div className="toy-card" data-aos="fade-right" data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="1000" >
+          <img
+            src="https://www.joblo.com/wp-content/uploads/2023/04/Fs0ObfgX0AMLSTL-1024x777.jpeg"
+            alt=""
+          />
+        </div>
+        <div className="toy-card" data-aos="fade-left" data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="1000" >
+          <img
+            src="https://www.joblo.com/wp-content/uploads/2023/04/Fs0ObfgX0AMLSTL-1024x777.jpeg"
+            alt=""
+          />
+        </div>
+        <div className="toy-card" data-aos="fade-right" data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="1000" >
+          <img
+            src="https://www.joblo.com/wp-content/uploads/2023/04/Fs0ObfgX0AMLSTL-1024x777.jpeg"
+            alt=""
+          />
+        </div>
+        <div className="toy-card" data-aos="fade-left" data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="1000" >
+          <img
+            src="https://www.joblo.com/wp-content/uploads/2023/04/Fs0ObfgX0AMLSTL-1024x777.jpeg"
+            alt=""
+          />
+        </div>
+        <div className="toy-card" data-aos="fade-right" data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="1000" >
+          <img
+            src="https://www.joblo.com/wp-content/uploads/2023/04/Fs0ObfgX0AMLSTL-1024x777.jpeg"
+            alt=""
+          />
+        </div>
+        <div className="toy-card" data-aos="fade-left" data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="1000" >
+          <img
+            src="https://www.joblo.com/wp-content/uploads/2023/04/Fs0ObfgX0AMLSTL-1024x777.jpeg"
+            alt=""
+          />
+        </div>
+        <div className="toy-card" data-aos="fade-right" data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="1000" >
+          <img
+            src="https://www.joblo.com/wp-content/uploads/2023/04/Fs0ObfgX0AMLSTL-1024x777.jpeg"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
